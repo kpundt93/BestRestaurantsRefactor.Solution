4fc8e6e5248500b24ae1268ebcf664e6463167be
Katie Pundt <kzpundt@gmail.com>
2022-01-05 09:24:28 -0800

    initial commit
    Co-authored-by: Anna Pittman <annaepittman@gmail.com>
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>

---
+++ BestRestaurants/Models/Restaurant.cs
@@ -0,0 +1,12 @@
+namespace BestRestaurants.Models
+{
+  public class Restaurant
+  {
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Location { get; set; }
+    public int CuisineId { get; set; }
+    public virtual Cuisine Cuisine { get; set; }
+  }
+}
---
+++ BestRestaurants/Models/RestaurantCuisine.cs
@@ -0,0 +1,12 @@
+namespace BestRestaurants.Models
+{
+  public class RestaurantCuisine
+  {
+    public int RestaurantCuisineId { get; set; }
+    public int RestaurantId { get; set; }
+    public int CuisineId { get; set; }
+
+    public virtual Restaurant Restaurant { get; set; }
+    public virtual Cuisine Cuisine { get; set; }
+  }
+}
